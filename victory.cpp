7f141ad22e17a79df73c3be5d35156a2ba4de30a
tom <tomfraisse.pro@gmail.com>
2023-12-18 01:08:51 +0100

    Add victory workfile

---
+++ victory.cpp
@@ -0,0 +1,9 @@
+//
+// Created by tfraisse on 17/12/23.
+//
+#include "victory.hpp"
+int victory(int matrix[7][6]){
+    int aligned_yellow_piece=0;
+    int aligned_red_piece=0;
+    bool stop=false;
+}
---
+++ victory.hpp
@@ -0,0 +1,9 @@
+//
+// Created by tfraisse on 17/12/23.
+//
+
+#ifndef CONNECT_FOUR_VICTORY_HPP
+#define CONNECT_FOUR_VICTORY_HPP
+#include "Window.hpp"
+int victory(int matrix[7][6]);
+#endif //CONNECT_FOUR_VICTORY_HPP
